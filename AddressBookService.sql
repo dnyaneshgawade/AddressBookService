8ebefbd0b09d076fb40ea923d5a585cd32337ea8
dnyana <dnyaneshgawade903@gmail.com>
2021-08-30 16:25:26 +0530

    [Dnyanesh] Add : Creating AddressBook table in AddressBookService Database

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -4,7 +4,17 @@ create database AddressBookService
 
 use AddressBookService
 
+--UC2- Ability to create a Address Book Table with first and last names, address, city, state, zip, phone number and email as its attributes
 
 
-
-
+create table AddressBook
+(
+	FirstName varchar(20) not null,
+	LastName varchar(20) not null,
+	Address varchar(20) not null,
+	City varchar(20) not null,
+	State varchar(20) not null,
+	Zip float not null,
+	PhoneNumber float not null,
+	Email varchar(50) not null
+)
